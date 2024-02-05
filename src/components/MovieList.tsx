f8248dd819d8d7aa38ccea0b4054d8fafefe18a8
newaritra <aritraroy14.ar@gmail.com>
2024-02-05 13:19:34 +0530

    -added env for the api key

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -13,7 +13,7 @@ export interface Movies {
 const getMovies = async () => {
   const data = await fetch("https://api.themoviedb.org/3/discover/movie", {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZTYxYTg3Y2RlYTE5OWQwZjQ3YThmNmY4ODQzNGY1ZiIsInN1YiI6IjY1YmYxMzFhMWRiYzg4MDE3YzFlNDI4MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2r79y0Fvu84PrgnoRFcSSiXMZIYbS1xexmyDaZLr_AQ`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
     },
   });
   const res = await data.json();
